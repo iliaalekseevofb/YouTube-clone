ff88560f26c4014c1cd26bf004aa6f0bf0437a6d
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-08-29 01:03:19 +0300

    Video thumbnails are showed now

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -14,7 +14,7 @@ const Feed = () => {
 
   return (
     <Stack sx={{flexDirection: {xs: 'column', md: 'row'}}}>
-      <Box sx={{height: {xs: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px: {xs: 0, md: 2}}}>
+      <Box sx={{height: {xs: 'auto', md: '88vh'}, borderRight: '1px solid #3d3d3d', px: {xs: 0, md: 2}}}>
         <Sidebar 
           selectedCategory={selectedCategory}
           setSelectedCategory={setSelectedCategory}
@@ -29,7 +29,7 @@ const Feed = () => {
       </Box>
       <Box 
         p={2}
-        sx={{overflowY: 'auto', height: '90vh', flex: 2}}
+        sx={{overflowY: 'auto', height: '84vh', flex: 2}}
       >
         <Typography
           variant='h4'
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Stack, Box } from '@mui/material';
+import { VideoCard, ChannelCard } from './';
 
 const Videos = ({videos}) => {
   return (
@@ -11,7 +12,8 @@ const Videos = ({videos}) => {
     >
       {videos.map((item, index) => (
         <Box key={index}>
-          
+          {item.id.videoId && <VideoCard video={item} />}
+          {/* {item.id.channelId && <ChannelCard channelDetail={item} />} */}
         </Box>
       ))}
     </Stack>
