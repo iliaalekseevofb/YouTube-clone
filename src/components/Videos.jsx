f868de8e8e73a4b9f5c91ab090196200eea6a226
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-08-29 02:13:50 +0300

    Channel card is done

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -13,7 +13,7 @@ const Videos = ({videos}) => {
       {videos.map((item, index) => (
         <Box key={index}>
           {item.id.videoId && <VideoCard video={item} />}
-          {/* {item.id.channelId && <ChannelCard channelDetail={item} />} */}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
         </Box>
       ))}
     </Stack>
