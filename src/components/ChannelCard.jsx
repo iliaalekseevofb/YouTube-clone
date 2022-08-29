3096dc261a94a121814169102a8abe3007e7f65f
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-08-29 12:05:51 +0300

    Search functionality is added

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import { demoProfilePicture } from '../utils/constants';
 
 const ChannelCard = ({channelDetail, marginTop}) => {
+  console.log(channelDetail)
   return (
     <Box
       sx={{
@@ -23,11 +24,11 @@ const ChannelCard = ({channelDetail, marginTop}) => {
         <CardContent
           sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center', color: '#fff'}}
         >
-          <CardMedia 
-            image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture} 
-            alt={channelDetail?.snippet?.title}
-            sx={{borderRadius: '50%', height: '180px', width: '180px', mb: 2, border: '1px solid #e3e3e3'}}
-          />
+        <CardMedia
+          image={demoProfilePicture}
+          alt={channelDetail?.snippet?.title}
+          sx={{ borderRadius: '50%', height: '180px', width: '180px', mb: 2, border: '1px solid #e3e3e3', background: 'transparent' }}
+        />
           <Typography variant='h6'>
             {channelDetail?.snippet?.title}
             <CheckCircle 
